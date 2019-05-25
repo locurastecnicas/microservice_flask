ebfeb35399adbdffcf5c84f592d49e7ab18301d8
locurastecnicas@gmail.com <locurastecnicas@gmail.com>
2019-05-25 08:50:16 +0200

    Pruebas iniciales con Flask.

--- basic.py
+++ basic.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return("Hello World!")
+@app.route("/login")
+def login():
+    return("Login page!!!")
+@app.route("/admin")
+def admin():
+    return("Admin page!!!!")
